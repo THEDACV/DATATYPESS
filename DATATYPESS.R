4bfdc63a980989ae6c71f76844f7cab0e1294460
chaithanya vishwamitra D A <163235188+THEDACV@users.noreply.github.com>
2024-09-02 15:02:03 +0530

    Add files via upload

---
+++ DATATYPESS.R
@@ -0,0 +1,16 @@
+radius=2L
+PI=3.14
+area=PI*radius*radius
+com=5+4i
+ch="NDRK college"
+var=TRUE
+#1.variable
+area
+#2.constant
+PI
+#3.data types
+class(radius)
+class(com)
+class(ch)
+class(var)
+class(area)
